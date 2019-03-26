a7507c96c94a08985c3c9f4712b2a6ab890a056b
shangeth <shangethrajaa@gmail.com>
2019-03-26 13:24:28 +0530

    data file

--- topic_modelling.py
+++ topic_modelling.py
@@ -5,7 +5,7 @@ from model import *
 
 def main(date):
 	path = '/content/nytimes_news_articles.txt'
-	nyTimesTemp, dates = preprocess_data('/content/nytimes_news_articles.txt')
+	nyTimesTemp, dates = preprocess_data('./data/nytimes_news_articles.txt')
 	date_indexes = get_index_dates(date, dates)
 	articles = get_articles_for_date(date_indexes, nyTimesTemp)
 	run_topic_modelling(articles)
